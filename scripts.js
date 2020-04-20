b9bd4456ae0fc81ce889b04ab722a3962a94ce29
mauro Leos <mauroleos94@gmail.com>
2020-04-20 08:55:54 -0500

    Worked on JS

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
 var buttonSelected = document.getElementsByClassName('number-buttons');
-var operations = document.querySelectorAll("operation-buttons");
+var operations = document.getElementsByClassName("operation-buttons");
 const allClear = document.getElementById('all-clear');
 const getTotal = document.getElementById("equals-button");
 const displayOutput = document.getElementById("display-output");
 const deleteNumber = document.getElementById("delte-button")
 
+console.log(operations)
 
-for (var i = 0; i < buttonSelected.length; i++) {
-    buttonSelected[i].addEventListener('click', getNumber);
-}
-function getNumber() {
-    alert('it works');
-}
 
-console.log(buttonSelected)
+
+
+
+
+
+// for (var i = 0; i < buttonSelected.length; i++) {
+//     buttonSelected[i].addEventListener('click', getNumber);
+// }
+// function getNumber() {
+//     alert ('it works')
+//     }
