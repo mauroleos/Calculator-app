ec4516f5c33597f3cbe94fd75e35945e54b5f2d5
mauro Leos <mauroleos94@gmail.com>
2020-04-22 20:28:02 -0500

    Worked on JS

--- scripts.js
+++ scripts.js
@@ -2,9 +2,6 @@
 // var operations = document.querySelectorAll(".operation-buttons");
 // const allClear = document.querySelector('.all-clear');
 // var displayOutput = document.querySelector(".display-output");
-let num1;
-let num2;
-let symbol;
 
 function allClear() {
     var displayOutput = document.querySelector(".display-output");
@@ -20,16 +17,6 @@ buttonSelected.forEach((button) => {
         } else {
             (displayOutput.innerHTML = displayOutput.innerHTML + e.target.innerHTML)
         }
-    })
-})
-
-var operations = document.querySelectorAll(".operation-buttons");
-operations.forEach((operation) => {
-    operation.addEventListener('click', (e) => {
-        var displayOutput = document.querySelector(".display-output");
-        var operand = e.target.value;
-        operand = symbol;
-        displayOutput.innerHTML = ''
     })
 })
 
