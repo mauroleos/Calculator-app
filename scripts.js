2e70c32e398dcaeff0c40265f0cae9ecafdc1495
mauro Leos <mauroleos94@gmail.com>
2020-04-27 00:00:56 -0500

    Worked on JS

--- scripts.js
+++ scripts.js
@@ -7,15 +7,10 @@ let num1 = null;
 let num2 = null;
 let operand = null;
 
-function allClear() {
-    var displayOutput = document.querySelector(".display-output");
-    displayOutput.innerHTML = ''
-}
-
-var buttonSelected = document.querySelectorAll(".number-buttons");
+var buttonSelected = document.querySelectorAll('.number-buttons');
 buttonSelected.forEach((button) => {
     button.addEventListener('click', (e) => {
-        var displayOutput = document.querySelector(".display-output");
+        var displayOutput = document.querySelector('.display-output');
         if (displayOutput.innerHTML === '' || displayOutput.innerHTML === '0') {
             (displayOutput.innerHTML = e.target.innerHTML)
         } else {
@@ -24,26 +19,55 @@ buttonSelected.forEach((button) => {
     })
 })
 
-var operations = document.querySelectorAll(".operation-buttons");
+var operations = document.querySelectorAll('.operation-buttons');
 operations.forEach((button) => {
     button.addEventListener('click', (e) => {
-        var displayOutput = document.querySelector(".display-output");
-        let operationsValue = e.target.innerHTML;
+        var displayOutput = document.querySelector('.display-output');
+        var operationsValue = e.target.innerHTML;
         operand = operationsValue;
         displayOutput.innerHTML = '';
     })
+});
 
-    console.log(operand)
-})
+var buttonValue = document.querySelectorAll('.number-buttons');
+var displayOutput = document.querySelector('.display-output');
+var number = displayOutput.innerHTML;
+buttonValue.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        if(operand === null) {
+            num1 = parseFloat(number)
+        } else if (num1 !== null && operand !== null) {
+            num2 = parseFloat(number);
+            // console.log(typeof(num1));
+        }
+    })
+});
 
+function allClear() {
+    var displayOutput = document.querySelector('.display-output');
+    displayOutput.innerHTML = ''
+}
 
-function deleteBtn() { }
+function equals() {
+    var operations = document.querySelectorAll('.operation-buttons');
+    var displayOutput = document.querySelector(".display-output");
+    let total = null;
 
-function equals() { }
+    switch (operations) {
+        case '÷':
+            total = sum1 / sum2;
+            break;
+        case '*':
+            total = sum1 * sum2;
+            break;
+        case '+':
+            total = sum1 + sum2;
+        case '-':
+            total = sum1 - sum2;
+        default:
+            return;
+    }
+    displayOutput.innerHTML = total
+}
 
-var operations = document.querySelectorAll(".operation-buttons")
-
-// switch(opertions) {
-//     case '*':
-
-// }
+function deleteBtn() {}
