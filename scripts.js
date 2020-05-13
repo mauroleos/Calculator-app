609850ba0cf990d482da2c575e4fce78d7ad0a8a
mauro Leos <mauroleos94@gmail.com>
2020-05-13 00:26:17 -0500

    Organized code

--- scripts.js
+++ scripts.js
@@ -6,14 +6,17 @@ var buttonSelected = document.querySelectorAll('.number-buttons');
 buttonSelected.forEach((button) => {
     button.addEventListener('click', (e) => {
         var displayOutput = document.querySelector('.display-output');
-        if (displayOutput.innerText === '' || displayOutput.innerText === '0' || (num1 !== null && num2 === null && operand !== null)) {
-            (displayOutput.innerText = e.target.innerText)
+
+        const isDisplayEmpty = displayOutput.innerText === '' || displayOutput.innerText === '0';
+        const isEquationCompletelyEmpty = num1 !== null && num2 === null && operand !== null;
+        if (isDisplayEmpty || isEquationCompletelyEmpty) {
+            displayOutput.innerText = e.target.innerText;
         } else {
-            (displayOutput.innerText = displayOutput.innerText + e.target.innerText)
+            displayOutput.innerText = displayOutput.innerText + e.target.innerText;
         }
         updatedNumberVariables();
     })
-})
+});
 
 const allClearBtn = document.querySelector('#all-clear-btn');
 allClearBtn.addEventListener('click', () => {
@@ -22,29 +25,14 @@ allClearBtn.addEventListener('click', () => {
     num1 = null;
     num2 = null;
     operand = null;
-})
+});
 
 var operations = document.querySelectorAll('.operation-buttons');
 operations.forEach((button) => {
     button.addEventListener('click', (e) => {
         var displayOutput = document.querySelector('.display-output');
         if (num1 !== null && num2 !== null && operand !== null) {
-            switch (operand) {
-                case '÷':
-                    num1 = num1 / num2;
-                    break;
-                case '*':
-                    num1 = num1 * num2;
-                    break;
-                case '+':
-                    num1 = num1 + num2;
-                    break;
-                case '-':
-                    num1 = num1 - num2;
-                    break;
-                default:
-                    return;
-            }
+            num1 = executeEquation();
             num2 = null;
             displayOutput.innerText = num1;
         } else {
@@ -57,43 +45,20 @@ operations.forEach((button) => {
 
 const equalsButton = document.querySelector('#equals-btn');
 equalsButton.addEventListener('click', () => {
+
+    num1 = executeEquation();
+    num2 = null;
+    operand = null;
+
     var displayOutput = document.querySelector(".display-output");
-    let total = null;
-
-    switch (operand) {
-        case '÷':
-            total = num1 / num2;
-            num1 = total;
-            num2 = null;
-            operand = null;
-            break;
-        case '*':
-            total = num1 * num2;
-            num1 = total;
-            num2 = null;
-            operand = null;
-            break;
-        case '+':
-            total = num1 + num2;
-            num1 = total;
-            num2 = null;
-            operand = null;
-            break;
-        case '-':
-            total = num1 - num2;
-            num1 = total;
-            num2 = null;
-            operand = null;
-            break;
-        default:
-            return;
-    }
-
-    total % 1 != 0 ? displayOutput.innerText = total.toFixed(3) : displayOutput.innerText = total;
-})
+    displayOutput.innerText = num1 % 1 != 0
+        ? num1.toFixed(3)
+        : num1;
+});
 
 const deleteBtn = document.querySelector('#delete-btn');
 deleteBtn.addEventListener('click', () => {
+    var displayOutput = document.querySelector('.display-output');
     let displayArray = Array.from(displayOutput.innerText);
     const trimmedArray = displayArray.splice(0, displayArray.length - 1);
     let finalDisplay = '';
@@ -102,9 +67,18 @@ deleteBtn.addEventListener('click', () => {
         const item = trimmedArray[i];
         finalDisplay += item;
     }
+
     displayOutput.innerText = finalDisplay;
     updatedNumberVariables();
-})
+});
+
+const decimal = document.querySelector('#decimal-btn');
+decimal.addEventListener('click', (e) => {
+    var displayOutput = document.querySelector('.display-output');
+    if (displayOutput.innerText.includes('.') === false) {
+        displayOutput.append('.');
+    }
+});
 
 function updatedNumberVariables() {
     var displayOutput = document.querySelector('.display-output');
@@ -115,13 +89,17 @@ function updatedNumberVariables() {
     }
 }
 
-const decimal = document.querySelector('#decimal-btn');
-var displayOutput = document.querySelector('.display-output');
-decimal.addEventListener('click', (e) => {
-    if (e.target.innerText === '.' && displayOutput.innerText.includes('.')) {
-        return;
-    } else {
-        displayOutput.append('.')
+function executeEquation() {
+    switch (operand) {
+        case '÷':
+            return num1 / num2;
+        case '*':
+            return num1 * num2;
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        default:
+            return;
     }
-
-})
+}
